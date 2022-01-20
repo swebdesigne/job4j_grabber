e662a65015f2f577043026e016f0ced989742652
Igor <swebdesigne@gmail.com>
2022-01-20 15:12:45 +0300

    Парсинг https://www.sql.ru/forum/job-offers/3

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -4,15 +4,16 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-import ru.job4j.grabber.utils.SqlRuDateTimeParser;
 
 import java.io.IOException;
 
 public class SqlRuParse {
-    public static void main(String[] args) throws IOException {
-        SqlRuDateTimeParser sql = new SqlRuDateTimeParser();
-        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
+    private int page = 0;
+
+    private void parsePage(String link) throws IOException {
+        Document doc = Jsoup.connect(link).get();
         Elements row = doc.select(".forumTable").select("tr");
+        String next = doc.select(".sort_options tbody tr td").select("a").eq(page++).attr("href");
         for (Element td : row) {
             System.out.println(String.format("Name %s, link %s, data %s",
                     td.child(1).children().text(),
@@ -20,5 +21,13 @@ public class SqlRuParse {
                     td.child(5).text()
             ));
         }
+        if (page < 5) {
+            parsePage(next);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        SqlRuParse s = new SqlRuParse();
+        s.parsePage("https://www.sql.ru/forum/job-offers/");
     }
 }
