56ffd15b6fad171f0ec5ede66d73ef83dc2358f0
Igor <swebdesigne@gmail.com>
2022-01-20 14:23:28 +0300

    Преобразование даты

--- src/main/java/ru/job4j/grabber/utils/SqlRuDateTimeParser.java
+++ src/main/java/ru/job4j/grabber/utils/SqlRuDateTimeParser.java
@@ -6,6 +6,8 @@ import java.time.LocalTime;
 import java.util.Map;
 
 public class SqlRuDateTimeParser implements DateTimeParser {
+     private static final String TODAY = "сегодня";
+     private static final String YESTERDAY = "вчера";
      private static final Map<String, String> MONTHS = Map.ofEntries(
              Map.entry("янв", "01"),
              Map.entry("фев", "02"),
@@ -23,31 +25,37 @@ public class SqlRuDateTimeParser implements DateTimeParser {
 
     @Override
     public LocalDateTime parse(String parse) {
+        LocalDate today = LocalDate.now();
         String[] data = parse.split(", ");
         String[] date = data[0].replace(",", "").split(" ");
         String[] time = data[1].split(":");
-        return switch (date[0]) {
-            case "сегодня" -> LocalDateTime.of(
-                    LocalDate.now(),
+        LocalDateTime localDateTime;
+        switch (date[0]) {
+            case TODAY : localDateTime = LocalDateTime.of(
+                    today,
                     LocalTime.of(
                             Integer.parseInt(time[0]),
                             Integer.parseInt(time[1])
                     )
             );
-            case "вчера" -> LocalDateTime.of(
+            break;
+            case YESTERDAY : localDateTime = LocalDateTime.of(
                     LocalDate.now().getYear(),
-                    LocalDate.now().getMonth(),
+                    today.minusDays(1).getMonth(),
                     LocalDate.now().getDayOfMonth() - 1,
                     Integer.parseInt(time[0]),
                     Integer.parseInt(time[1])
             );
-            default -> LocalDateTime.of(
+            break;
+            default : localDateTime = LocalDateTime.of(
                     Integer.parseInt(date[2]),
                     Integer.parseInt(MONTHS.get(date[1])),
                     Integer.parseInt(date[0]),
                     Integer.parseInt(time[0]),
                     Integer.parseInt(time[1])
             );
-        };
+            break;
+        }
+        return localDateTime;
     }
 }
