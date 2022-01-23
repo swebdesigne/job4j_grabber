10f12f0fbead151250cb695513aa850bb2afd4bd
swebdesigne <swebdesigne@gmail.com>
2022-01-23 15:50:57 +0300

    Загрузка деталей поста

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -4,8 +4,12 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import ru.job4j.grabber.Post;
+import ru.job4j.grabber.utils.SqlRuDateTimeParser;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.logging.SimpleFormatter;
 
 public class SqlRuParse {
     private static final int END = 5;
@@ -24,8 +28,16 @@ public class SqlRuParse {
         }
     }
 
+    private String[] loadingPost(String link) throws IOException {
+        Document doc = Jsoup.connect(link).get();
+        String desc = doc.select(".msgBody").eq(1).text();
+        String date = doc.select(".msgFooter").eq(0).text().split("\\[")[0].trim();
+        return new String[] {desc, date};
+    }
+
     public static void main(String[] args) throws IOException {
         SqlRuParse s = new SqlRuParse();
         s.parsePage("https://www.sql.ru/forum/job-offers/");
+        s.loadingPost("https://www.sql.ru/forum/1325330/lidy-be-fe-senior-cistemnye-analitiki-qa-i-devops-moskva-do-200t");
     }
 }
